2a258a992c243bb4b189093d3827c3a3a03abbb9
ohall <ohall@grassroots-tech.com>
2013-09-01 15:44:42 -0400

    Trying to get EmailComposer to work..

--- EmailComposer.m
+++ EmailComposer.m
@@ -1,104 +1,194 @@
-//
+/**
+ * Email Composer plugin for PhoneGap/Cordova
+ * window.plugins.emailComposer
+ *
+ * Unified and updated API to be cross-platform by Gal Cohen in 2013.
+ * galcohen26@gmail.com
+ * https://github.com/GalCohen
+ *
+ * Originally created by Guido Sabatini in 2012.
+ * Original code from:
+ * android: https://github.com/phonegap/phonegap-plugins/tree/master/Android/EmailComposerWithAttachments
+ * ios https://github.com/phonegap/phonegap-plugins/tree/5cf45fcade4989668e95a6d34630d2021c45291a/iOS/SMSComposer
+ * js: https://github.com/phonegap/phonegap-plugins/blob/5cf45fcade4989668e95a6d34630d2021c45291a/iOS/SMSComposer/SMSComposer.js
+ *
+ */
+
 //  EmailComposer.m
-// 
 //
-//  Created by Jesse MacFadyen on 10-04-05.
-//  Copyright 2010 Nitobi. All rights reserved.
-//
+//  Version 2.1
+
+#define RETURN_CODE_EMAIL_CANCELLED 0
+#define RETURN_CODE_EMAIL_SAVED 1
+#define RETURN_CODE_EMAIL_SENT 2
+#define RETURN_CODE_EMAIL_FAILED 3
+#define RETURN_CODE_EMAIL_NOTSENT 4
 
 #import "EmailComposer.h"
+#import <MobileCoreServices/MobileCoreServices.h>
 
+@interface EmailComposer ()
 
+-(void) showEmailComposerWithParameters:(NSDictionary*)parameters;
+-(void) returnWithCode:(int)code;
+-(NSString *) getMimeTypeFromFileExtension:(NSString *)extension;
+
+@end
 
 @implementation EmailComposer
 
-- (void) showEmailComposer:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options
-{	
-	// NSUInteger argc = [arguments count];
-	
-	NSString* toRecipientsString = [options valueForKey:@"toRecipients"];
-	NSString* ccRecipientsString = [options valueForKey:@"ccRecipients"];
-	NSString* bccRecipientsString = [options valueForKey:@"bccRecipients"];
-	NSString* subject = [options valueForKey:@"subject"];
-	NSString* body = [options valueForKey:@"body"];
-	NSString* isHTML = [options valueForKey:@"bIsHTML"];
-	
-    MFMailComposeViewController *picker = [[MFMailComposeViewController alloc] init];
-    picker.mailComposeDelegate = self;
+// UNCOMMENT THIS METHOD if you want to use the plugin with versions of cordova < 2.2.0
+//- (void) showEmailComposer:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options {
+//    NSDictionary *parameters = [NSDictionary dictionaryWithObjectsAndKeys:
+//                                [options valueForKey:@"toRecipients"], @"toRecipients",
+//                                [options valueForKey:@"ccRecipients"], @"ccRecipients",
+//                                [options valueForKey:@"bccRecipients"], @"bccRecipients",
+//                                [options valueForKey:@"subject"], @"subject",
+//                                [options valueForKey:@"body"], @"body",
+//                                [options valueForKey:@"bIsHTML"], @"bIsHTML",
+//                                [options valueForKey:@"attachments"], @"attachments",
+//                                nil];
+//    [self showEmailComposerWithParameters:parameters];
+//}
+
+// COMMENT THIS METHOD if you want to use the plugin with versions of cordova < 2.2.0
+- (void) showEmailComposer:(CDVInvokedUrlCommand*)command {
+    NSDictionary *parameters = [command.arguments objectAtIndex:0];
+    [self showEmailComposerWithParameters:parameters];
+}
+
+-(void) showEmailComposerWithParameters:(NSDictionary*)parameters {
     
-	// Set subject
-	if(subject != nil)
-		[picker setSubject:subject];
-	// set body
-	if(body != nil)
-	{
-		if(isHTML != nil && [isHTML boolValue])
-		{
-			[picker setMessageBody:body isHTML:YES];
-		}
-		else
-		{
-			[picker setMessageBody:body isHTML:NO];
-		}
-	}
-	
-	// Set recipients
-	if(toRecipientsString != nil)
-	{
-		[picker setToRecipients:[ toRecipientsString componentsSeparatedByString:@","]];
-	}
-	if(ccRecipientsString != nil)
-	{
-		[picker setCcRecipients:[ ccRecipientsString componentsSeparatedByString:@","]]; 
-	}
-	if(bccRecipientsString != nil)
-	{
-		[picker setBccRecipients:[ bccRecipientsString componentsSeparatedByString:@","]];
-	}
-	
-    // Attach an image to the email
-	// NSString *path = [[NSBundle mainBundle] pathForResource:@"rainy" ofType:@"png"];
-	//  NSData *myData = [NSData dataWithContentsOfFile:path];
-	//  [picker addAttachmentData:myData mimeType:@"image/png" fileName:@"rainy"];
+    MFMailComposeViewController *mailComposer = [[MFMailComposeViewController alloc] init];
+    mailComposer.mailComposeDelegate = self;
     
-    if (picker != nil) {
-        [[ super appViewController ] presentModalViewController:picker animated:YES];
+	// set subject
+    @try {
+        NSString* subject = [parameters objectForKey:@"subject"];
+        if (subject) {
+            [mailComposer setSubject:subject];
+        }
     }
-    [picker release];
+    @catch (NSException *exception) {
+        NSLog(@"EmailComposer - Cannot set subject; error: %@", exception);
+    }
+    
+    // set body
+    @try {
+        NSString* body = [parameters objectForKey:@"body"];
+        BOOL isHTML = [[parameters objectForKey:@"bIsHTML"] boolValue];
+        if(body) {
+            [mailComposer setMessageBody:body isHTML:isHTML];
+        }
+    }
+    @catch (NSException *exception) {
+        NSLog(@"EmailComposer - Cannot set body; error: %@", exception);
+    }
+    
+	// Set recipients
+    @try {
+        NSArray* toRecipientsArray = [parameters objectForKey:@"toRecipients"];
+        if(toRecipientsArray) {
+            [mailComposer setToRecipients:toRecipientsArray];
+        }
+    }
+    @catch (NSException *exception) {
+        NSLog(@"EmailComposer - Cannot set TO recipients; error: %@", exception);
+    }
+    
+    @try {
+        NSArray* ccRecipientsArray = [parameters objectForKey:@"ccRecipients"];
+        if(ccRecipientsArray) {
+            [mailComposer setCcRecipients:ccRecipientsArray];
+        }
+    }
+    @catch (NSException *exception) {
+        NSLog(@"EmailComposer - Cannot set CC recipients; error: %@", exception);
+    }
+    
+    @try {
+        NSArray* bccRecipientsArray = [parameters objectForKey:@"bccRecipients"];
+        if(bccRecipientsArray) {
+            [mailComposer setBccRecipients:bccRecipientsArray];
+        }
+    }
+    @catch (NSException *exception) {
+        NSLog(@"EmailComposer - Cannot set BCC recipients; error: %@", exception);
+    }
+    
+	@try {
+        int counter = 1;
+        NSArray *attachmentPaths = [parameters objectForKey:@"attachments"];
+        if (attachmentPaths) {
+            for (NSString* path in attachmentPaths) {
+                @try {
+                    NSData *data = [[NSFileManager defaultManager] contentsAtPath:path];
+                    [mailComposer addAttachmentData:data mimeType:[self getMimeTypeFromFileExtension:[path pathExtension]] fileName:[NSString stringWithFormat:@"attachment%d.%@", counter, [path pathExtension]]];
+                    counter++;
+                }
+                @catch (NSException *exception) {
+                    NSLog(@"Cannot attach file at path %@; error: %@", path, exception);
+                }
+            }
+        }
+    }
+    @catch (NSException *exception) {
+        NSLog(@"EmailComposer - Cannot set attachments; error: %@", exception);
+    }
+    
+    if (mailComposer != nil) {
+        [self.viewController presentModalViewController:mailComposer animated:YES];
+    } else {
+        [self returnWithCode:RETURN_CODE_EMAIL_NOTSENT];
+    }
+    [mailComposer release];
 }
 
 
-// Dismisses the email composition interface when users tap Cancel or Send. Proceeds to update the message field with the result of the operation.
-- (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error 
-{   
+// Dismisses the email composition interface when users tap Cancel or Send.
+// Proceeds to update the message field with the result of the operation.
+- (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error {
     // Notifies users about errors associated with the interface
 	int webviewResult = 0;
-	
-    switch (result)
-    {
+    
+    switch (result) {
         case MFMailComposeResultCancelled:
-			webviewResult = 0;
+			webviewResult = RETURN_CODE_EMAIL_CANCELLED;
             break;
         case MFMailComposeResultSaved:
-			webviewResult = 1;
+			webviewResult = RETURN_CODE_EMAIL_SAVED;
             break;
         case MFMailComposeResultSent:
-			webviewResult =2;
+			webviewResult =RETURN_CODE_EMAIL_SENT;
             break;
         case MFMailComposeResultFailed:
-            webviewResult = 3;
+            webviewResult = RETURN_CODE_EMAIL_FAILED;
             break;
         default:
-			webviewResult = 4;
+			webviewResult = RETURN_CODE_EMAIL_NOTSENT;
             break;
     }
-	
-    [[ super appViewController ] dismissModalViewControllerAnimated:YES];
-	
-	NSString* jsString = [[NSString alloc] initWithFormat:@"window.plugins.emailComposer._didFinishWithResult(%d);",webviewResult];
-	[self writeJavascript:jsString];
-	[jsString release];
-	
+    
+    [controller dismissModalViewControllerAnimated:YES];
+    [self returnWithCode:webviewResult];
+}
+
+// Call the callback with the specified code
+-(void) returnWithCode:(int)code {
+    [self writeJavascript:[NSString stringWithFormat:@"window.plugins.emailComposer._didFinishWithResult(%d);", code]];
+}
+
+// Retrieve the mime type from the file extension
+-(NSString *) getMimeTypeFromFileExtension:(NSString *)extension {
+    if (!extension)
+        return nil;
+    CFStringRef pathExtension, type;
+    // Get the UTI from the file's extension
+    pathExtension = (CFStringRef)extension;
+    type = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, pathExtension, NULL);
+    
+    // Converting UTI to a mime type
+    return (NSString *)UTTypeCopyPreferredTagWithClass(type, kUTTagClassMIMEType);
 }
 
 @end
