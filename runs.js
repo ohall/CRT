b1ad5e717f2eb293d392106a6c11876afbb9ded5
ohall <ohall@grassroots-tech.com>
2013-09-09 16:21:10 -0400

    http instead

--- runs.js
+++ runs.js
@@ -17,48 +17,48 @@ function getRunPaths(){
             cities:[
             {
                 city:'Austin',
-                runs:{ 'indy':'https://www.active.com/running-membership/austin-tx/city-running-tours---austin-personalized-running-tours-2017'}
+                runs:{ 'indy':'http://www.active.com/running-membership/austin-tx/city-running-tours---austin-personalized-running-tours-2017'}
             },
             {
                 city:'Boston',
-                runs:{ 'indy':'https://www.active.com/cycling-membership/boston-ma/city-running-tours---boston-personalized-running-tours-2017'}
+                runs:{ 'indy':'http://www.active.com/cycling-membership/boston-ma/city-running-tours---boston-personalized-running-tours-2017'}
             },
             {
                 city:'Chicago',
-                runs:{ 'indy':'https://www.active.com/cycling-membership/chicago-il/city-running-tours---chicago-personalized-running-tours-2017'}
+                runs:{ 'indy':'http://www.active.com/cycling-membership/chicago-il/city-running-tours---chicago-personalized-running-tours-2017'}
             },
             {
                 city:'Denver',
-                runs:{'indy':'https://www.active.com/running-membership/denver-co/city-running-tours---denver-personalized-running-tours-2018'}
+                runs:{'indy':'http://www.active.com/running-membership/denver-co/city-running-tours---denver-personalized-running-tours-2018'}
             },
             {
                 city:'Minneapolis',
-                runs:{'indy':'https://www.active.com/running-membership/minneapolis-mn/city-running-tours---minneapolis-personalized-running-tours-2017'}
+                runs:{'indy':'http://www.active.com/running-membership/minneapolis-mn/city-running-tours---minneapolis-personalized-running-tours-2017'}
             },
             {
                 city:'New York',
-                runs:{'indy':'https://www.active.com/running-membership/new-york-ny/city-running-tours---new-york-city-personalized-running-tours-2017'}
+                runs:{'indy':'http://www.active.com/running-membership/new-york-ny/city-running-tours---new-york-city-personalized-running-tours-2017'}
             },
             {
                 city:'Philadelphia',
-                runs:{'indy':'https://www.active.com/cycling-membership/philadelphia-pa/city-running-tours---philadelphia-personalized-running-tours-2017'}
+                runs:{'indy':'http://www.active.com/cycling-membership/philadelphia-pa/city-running-tours---philadelphia-personalized-running-tours-2017'}
             },
             {
                 city:'Portland',
-                runs:{'indy':'https://www.active.com/running-membership/portland-or/city-running-tours---portland-personalized-running-tours-2017'}
+                runs:{'indy':'http://www.active.com/running-membership/portland-or/city-running-tours---portland-personalized-running-tours-2017'}
             },
             {
 
                 city:'San Francisco',
-                runs:{'indy':'https://www.active.com/cycling-membership/san-francisco-ca/city-running-tours---san-francisco-personalized-running-tours-2017'}
+                runs:{'indy':'http://www.active.com/cycling-membership/san-francisco-ca/city-running-tours---san-francisco-personalized-running-tours-2017'}
             },
             {
                 city:'Seattle',
-                runs:{'indy':'https://www.active.com/cycling-membership/seattle-wa/city-running-tours---seattle-personalized-running-tours-2017'}
+                runs:{'indy':'http://www.active.com/cycling-membership/seattle-wa/city-running-tours---seattle-personalized-running-tours-2017'}
             },
             {
                 city:'Washington DC',
-                runs:{'indy':'https://www.active.com/running-membership/washington-dc/city-running-tours---washington-dc-personalized-running-tours-2017'}
+                runs:{'indy':'http://www.active.com/running-membership/washington-dc/city-running-tours---washington-dc-personalized-running-tours-2017'}
             }
             ]
     },
